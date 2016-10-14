d69948d2ac960a5d5d6357e7b40bc266e618f5c2
a-sipnevski <a.sipnevski@gmail.com>
2016-10-14 12:15:49 +0300

    asdf

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,7 @@
+ShopifyApp.configure do |config|
+
+  config.api_key = "f592da09f6fdf102826197744e092108"
+  config.secret = "9ded3039ad5530d3c83ec9323c2d617c"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
